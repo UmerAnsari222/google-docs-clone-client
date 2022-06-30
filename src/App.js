fc83936b97ebb7eb96e5313cceaf0d990d4869a2
UmerAnsari222 <89651067+UmerAnsari222@users.noreply.github.com>
2022-06-29 22:30:33 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Editor from "./components/Editor";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to={`/doc/${uuid()}`} />} />
+        <Route path="/doc/:id" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
